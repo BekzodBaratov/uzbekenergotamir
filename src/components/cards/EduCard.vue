3e68d401b50efd375f9f85e77da3b1e304fe08e6
Bekzod <bekzodbaratov334@gmail.com>
2023-05-13 11:38:23 +0500

    edu page end

---
+++ src/components/cards/EduCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="educard group">
+    <div class="tag px-4 pt-5 pb-16 bg-white duration-200 group-hover:bg-blue-600 rounded-lg">
+      <div class="relative">
+        <div class="absolute top-0 right-0">
+          <span class="text-7xl text-gray-600 duration-200 group-hover:text-gray-100 opacity-20 font-bold">{{
+            data.number
+          }}</span>
+        </div>
+      </div>
+      <h3 class="text-primary2 pr-20 duration-200 group-hover:text-white">{{ data.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["data"]);
+const data = computed(() => props.data);
+</script>
+
+<style scoped>
+.educard {
+  filter: drop-shadow(-1px 6px 6px rgba(0, 0, 0, 0.2));
+}
+.tag {
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  clip-path: polygon(0 0, 100% 0%, 100% 75%, 75% 100%, 0 100%);
+}
+</style>
--- src/views/Educational.vue
+++ src/views/Educational.vue
@@ -1,1 +1,40 @@
-<template>Educational</template>
+<template>
+  <section class="educational relative py-8 md:py-16">
+    <div class="container">
+      <h2 class="text-big">Учебный центр</h2>
+      <p class="text-primary3 pb-6">Обучение, переподготовка и повышение квалификации персонала</p>
+      <div class="cards grid grid-cols-3 gap-6 py-8">
+        <EducationalCard v-for="data in eduArr" :key="data.id" :data="data" />
+      </div>
+    </div>
+
+    <div class="eduClipPath1 absolute inset-0 bg-yellowish -z-50"></div>
+    <div class="eduClipPath2 absolute inset-0 bg-bluer -z-50"></div>
+  </section>
+</template>
+
+<script setup>
+import EducationalCard from "/src/components/cards/EduCard.vue";
+
+const eduArr = [
+  { id: 1, title: "Электрогазосварщик (ручная сварка, в среде защитного газа аргон)", number: "01" },
+  { id: 2, title: "Электромонтер по ремонту электрооборудования", number: "02" },
+  { id: 3, title: "Обмотчик элемента электромашин", number: "03" },
+  { id: 4, title: "Слесарь по ремонту котельных ТЭС и пылегазоочистных цехов", number: "04" },
+  { id: 5, title: "Слесарь по ремонту паротурбинного оборудования тепловых электростанций", number: "05" },
+  { id: 6, title: "Станочник широкого профиля", number: "06" },
+  { id: 7, title: "Слесарь контрольно-измерительных приборов и автоматики", number: "07" },
+  { id: 8, title: "Монтажник", number: "08" },
+  { id: 9, title: "Оператор станков с числовым програмным управлением", number: "09" },
+  { id: 10, title: "Наладчик энергооборудования", number: "10" },
+];
+</script>
+
+<style scoped>
+.eduClipPath1 {
+  clip-path: polygon(0 20px, 0 0, 100% 0, 100% 20px, 50% 50%);
+}
+.eduClipPath2 {
+  clip-path: polygon(100% 20px, 100% 100%, 98% 100%, 50% 50%);
+}
+</style>
